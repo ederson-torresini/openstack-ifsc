be37f04d85d86189b38120506825a94ec2e702ae
Ederson Torresini <boidacarapreta@gmail.com>
2014-08-21 00:40:02 -0300

    About the issue #1, instead of using Ceph for storing temporary files between Glance and Cinder, a LV will be used, because it’s a local process need - no need to involve others nodes.

--- puppet/modules/openstack-cinder-node/manifests/init.pp
+++ puppet/modules/openstack-cinder-node/manifests/init.pp
@@ -11,5 +11,30 @@ class openstack-cinder-node {
 		subscribe => File['cinder.conf'],
 	}
 
+	# Issue #1: Create space, to allow conversion between glance and cinder, for large files.
+	exec { 'lvcreate conversion':
+		command => '/sbin/lvcreate -L 100G -n conversion openstack',
+		unless => '/sbin/lvdisplay | /bin/grep -q conversion',
+		require => Exec['/sbin/vgcreate openstack /dev/sda3'],
+	}
+
+	exec { 'mkfs conversion':
+		command => '/sbin/mkfs.ext4 /dev/openstack/conversion',
+		subscribe => Exec['lvcreate conversion'],
+		refreshonly => true,
+	}
+
+	exec { 'fstab conversion':
+		command => "/bin/echo /dev/openstack/conversion /var/lib/cinder/conversion ext4 rw,nodev,noexec,nosuid 0 2 >> /etc/fstab",
+		unless => '/bin/grep -q /var/lib/cinder/conversion /etc/fstab',
+		require => Exec['mkfs conversion'],
+	}
+
+	exec { 'mount conversion':
+		command => '/bin/mount /var/lib/cinder/conversion',
+		unless => '/bin/mount | /bin/grep -q conversion',
+		require => Exec['fstab conversion'],
+	}
+
 }
 

f635a121ed8de386d1198515b64922483c1a4608
Ederson Torresini <boidacarapreta@gmail.com>
2015-03-26 16:56:20 -0300

    Keepalived up and running. Keepalived and HAproxy works pretty well. I've found a couple issues with Keystone and MySQL - for instance, too many connections -, but soon I'll change to Galera :)

---
+++ puppet/modules/keepalived/manifests/init.pp
@@ -0,0 +1,43 @@
+class keepalived {
+
+	file { 'sysctl-keepalived.conf':
+		path => '/etc/sysctl-keepalived.conf',
+		source => 'puppet:///modules/keepalived/sysctl-keepalived.conf',
+		owner => root,
+		group => root,
+		mode => 0644,
+	}
+
+	exec { 'sysctl:sysctl-keepalived.conf':
+		command => '/sbin/sysctl -p /etc/sysctl-keepalived.conf',
+		require => File['sysctl-keepalived.conf'],
+	}
+
+	package { 'keepalived':
+		ensure => installed,
+	}
+
+	$source = $hostname ? {
+		'openstack0' => 'puppet:///modules/keepalived/keepalived-openstack0.conf',
+		'openstack1' => 'puppet:///modules/keepalived/keepalived-openstack1.conf',
+		'openstack2' => 'puppet:///modules/keepalived/keepalived-openstack2.conf',
+		'openstack3' => 'puppet:///modules/keepalived/keepalived-openstack3.conf',
+	}
+
+	file { 'keepalived.conf':
+		path => '/etc/keepalived/keepalived.conf',
+		source => $source,
+		owner => root,
+		group => root,
+		mode => 0644,
+		require => Package['keepalived'],
+	}
+
+	service { 'keepalived':
+		ensure => running,
+		enable => true,
+		require => Exec['sysctl:sysctl-keepalived.conf'],
+		subscribe => File['keepalived.conf'],
+	}
+
+}

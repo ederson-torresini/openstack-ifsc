267ea4c984118e3443c17e24c4789861690428ea
Ederson Torresini <boidacarapreta@gmail.com>
2015-07-14 14:43:18 -0300

    Horizon Dashboard moved to openstack3. Updated DNS and roteador:/etc/hosts.

--- puppet/modules/openstack-horizon/files/local_settings.py
+++ puppet/modules/openstack-horizon/files/local_settings.py
@@ -6,26 +6,31 @@ from openstack_dashboard import exceptions
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
+# https://wiki.openstack.org/wiki/ReleaseNotes/Kilo#Key_New_Features_4
+LAUNCH_INSTANCE_NG_ENABLED = True
+
 HORIZON_CONFIG = {
-    'dashboards': ('project', 'admin', 'settings',),
-    'default_dashboard': 'project',
-    'user_home': 'openstack_dashboard.views.get_user_home',
-    'ajax_queue_limit': 10,
-    'auto_fade_alerts': {
-        'delay': 3000,
-        'fade_duration': 1500,
-        'types': ['alert-success', 'alert-info']
+	'dashboards': ('project', 'admin', 'settings',),
+	'default_dashboard': 'project',
+	'user_home': 'openstack_dashboard.views.get_user_home',
+	'ajax_queue_limit': 10,
+	'auto_fade_alerts': {
+		'delay': 3000,
+		'fade_duration': 1500,
+		'types': ['alert-success', 'alert-info']
+	},
+	'help_url': "http://docs.openstack.org",
+	'exceptions': {
+        'recoverable': exceptions.RECOVERABLE,
+        'not_found': exceptions.NOT_FOUND,
+        'unauthorized': exceptions.UNAUTHORIZED
     },
-    'help_url': "http://docs.openstack.org",
-    'exceptions': {'recoverable': exceptions.RECOVERABLE,
-                   'not_found': exceptions.NOT_FOUND,
-                   'unauthorized': exceptions.UNAUTHORIZED},
 }
 
 LOCAL_PATH = os.path.dirname(os.path.abspath(__file__))
 
 from horizon.utils import secret_key
-SECRET_KEY = secret_key.generate_or_read_from_file('/var/lib/openstack-dashboard/secret_key')
+SECRET_KEY = secret_key.generate_or_read_from_file('/etc/openstack-dashboard/secret_key')
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
 CACHES = {
@@ -49,44 +54,48 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 OPENSTACK_HOST = "keystone.openstack.sj.ifsc.edu.br"
 OPENSTACK_KEYSTONE_DEFAULT_ROLE = "_member_"
 
+# Based on https://docs.hpcloud.com/helion/openstack/1.1/services/identity/configure/
+OPENSTACK_API_VERSIONS = { "identity": 3, }
+OPENSTACK_KEYSTONE_URL = "http://%s:5000/v3" % OPENSTACK_HOST
+
 OPENSTACK_KEYSTONE_BACKEND = {
-    'name': 'native',
-    'can_edit_user': True,
-    'can_edit_group': True,
-    'can_edit_project': True,
-    'can_edit_domain': True,
-    'can_edit_role': True
+	'name': 'native',
+	'can_edit_user': True,
+	'can_edit_group': True,
+	'can_edit_project': True,
+	'can_edit_domain': True,
+	'can_edit_role': True
 }
 
 OPENSTACK_HYPERVISOR_FEATURES = {
-    'can_set_mount_point': False,
-    'can_set_password': False,
+	'can_set_mount_point': False,
+	'can_set_password': False,
 }
 
 OPENSTACK_CINDER_FEATURES = {
-    'enable_backup': True,
+	'enable_backup': True,
 }
 
 OPENSTACK_NEUTRON_NETWORK = {
-    'enable_lb': True,
-    'enable_firewall': True,
-    'enable_quotas': True,
-    'enable_router': True,
-    'enable_ipv6': True,
-    'enable_distributed_router': False,
-    'enable_ha_router': False,
-    'enable_vpn': True,
-    'profile_support': None,
-    'supported_provider_types': ['*'],
+	'enable_lb': True,
+	'enable_firewall': False,
+	'enable_quotas': True,
+	'enable_router': True,
+	'enable_ipv6': True,
+	'enable_distributed_router': False,
+	'enable_ha_router': False,
+	'enable_vpn': False,
+	'profile_support': None,
+	'supported_provider_types': ['*'],
 }
 
 IMAGE_CUSTOM_PROPERTY_TITLES = {
-    "architecture": _("Architecture"),
-    "kernel_id": _("Kernel ID"),
-    "ramdisk_id": _("Ramdisk ID"),
-    "image_state": _("Euca2ools state"),
-    "project_id": _("Project ID"),
-    "image_type": _("Image Type")
+	"architecture": _("Architecture"),
+	"kernel_id": _("Kernel ID"),
+	"ramdisk_id": _("Ramdisk ID"),
+	"image_state": _("Euca2ools state"),
+	"project_id": _("Project ID"),
+	"image_type": _("Image Type")
 }
 
 API_RESULT_LIMIT = 1000
@@ -94,220 +103,220 @@ API_RESULT_PAGE_SIZE = 20
 TIME_ZONE = "UTC"
 
 LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'null': {
-            'level': 'DEBUG',
-            'class': 'django.utils.log.NullHandler',
-        },
-        'console': {
-            'level': 'INFO',
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'loggers': {
-        'django.db.backends': {
-            'handlers': ['null'],
-            'propagate': False,
-        },
-        'requests': {
-            'handlers': ['null'],
-            'propagate': False,
-        },
-        'horizon': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': False,
-        },
-        'openstack_dashboard': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': False,
-        },
-        'novaclient': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': False,
-        },
-        'cinderclient': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': False,
-        },
-        'keystoneclient': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': False,
-        },
-        'glanceclient': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': False,
-        },
-        'neutronclient': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': False,
-        },
-        'heatclient': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': False,
-        },
-        'ceilometerclient': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': False,
-        },
-        'troveclient': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': False,
-        },
-        'swiftclient': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': False,
-        },
-        'openstack_auth': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': False,
-        },
-        'nose.plugins.manager': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': False,
-        },
-        'django': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': False,
-        },
-        'iso8601': {
-            'handlers': ['null'],
-            'propagate': False,
-        },
-        'scss': {
-            'handlers': ['null'],
-            'propagate': False,
-        },
-    }
+	'version': 1,
+	'disable_existing_loggers': False,
+	'handlers': {
+		'null': {
+			'level': 'DEBUG',
+			'class': 'django.utils.log.NullHandler',
+		},
+		'console': {
+			'level': 'INFO',
+			'class': 'logging.StreamHandler',
+		},
+	},
+	'loggers': {
+		'django.db.backends': {
+			'handlers': ['null'],
+			'propagate': False,
+		},
+		'requests': {
+			'handlers': ['null'],
+			'propagate': False,
+		},
+		'horizon': {
+			'handlers': ['console'],
+			'level': 'DEBUG',
+			'propagate': False,
+		},
+		'openstack_dashboard': {
+			'handlers': ['console'],
+			'level': 'DEBUG',
+			'propagate': False,
+		},
+		'novaclient': {
+			'handlers': ['console'],
+			'level': 'DEBUG',
+			'propagate': False,
+		},
+		'cinderclient': {
+			'handlers': ['console'],
+			'level': 'DEBUG',
+			'propagate': False,
+		},
+		'keystoneclient': {
+			'handlers': ['console'],
+			'level': 'DEBUG',
+			'propagate': False,
+		},
+		'glanceclient': {
+			'handlers': ['console'],
+			'level': 'DEBUG',
+			'propagate': False,
+		},
+		'neutronclient': {
+			'handlers': ['console'],
+			'level': 'DEBUG',
+			'propagate': False,
+		},
+		'heatclient': {
+			'handlers': ['console'],
+			'level': 'DEBUG',
+			'propagate': False,
+		},
+		'ceilometerclient': {
+			'handlers': ['console'],
+			'level': 'DEBUG',
+			'propagate': False,
+		},
+		'troveclient': {
+			'handlers': ['console'],
+			'level': 'DEBUG',
+			'propagate': False,
+		},
+		'swiftclient': {
+			'handlers': ['console'],
+			'level': 'DEBUG',
+			'propagate': False,
+		},
+		'openstack_auth': {
+			'handlers': ['console'],
+			'level': 'DEBUG',
+			'propagate': False,
+		},
+		'nose.plugins.manager': {
+			'handlers': ['console'],
+			'level': 'DEBUG',
+			'propagate': False,
+		},
+		'django': {
+			'handlers': ['console'],
+			'level': 'DEBUG',
+			'propagate': False,
+		},
+		'iso8601': {
+			'handlers': ['null'],
+			'propagate': False,
+		},
+		'scss': {
+			'handlers': ['null'],
+			'propagate': False,
+		},
+	}
 }
 
 SECURITY_GROUP_RULES = {
-    'all_tcp': {
-        'name': _('All TCP'),
-        'ip_protocol': 'tcp',
-        'from_port': '1',
-        'to_port': '65535',
-    },
-    'all_udp': {
-        'name': _('All UDP'),
-        'ip_protocol': 'udp',
-        'from_port': '1',
-        'to_port': '65535',
-    },
-    'all_icmp': {
-        'name': _('All ICMP'),
-        'ip_protocol': 'icmp',
-        'from_port': '-1',
-        'to_port': '-1',
-    },
-    'ssh': {
-        'name': 'SSH',
-        'ip_protocol': 'tcp',
-        'from_port': '22',
-        'to_port': '22',
-    },
-    'smtp': {
-        'name': 'SMTP',
-        'ip_protocol': 'tcp',
-        'from_port': '25',
-        'to_port': '25',
-    },
-    'dns': {
-        'name': 'DNS',
-        'ip_protocol': 'tcp',
-        'from_port': '53',
-        'to_port': '53',
-    },
-    'http': {
-        'name': 'HTTP',
-        'ip_protocol': 'tcp',
-        'from_port': '80',
-        'to_port': '80',
-    },
-    'pop3': {
-        'name': 'POP3',
-        'ip_protocol': 'tcp',
-        'from_port': '110',
-        'to_port': '110',
-    },
-    'imap': {
-        'name': 'IMAP',
-        'ip_protocol': 'tcp',
-        'from_port': '143',
-        'to_port': '143',
-    },
-    'ldap': {
-        'name': 'LDAP',
-        'ip_protocol': 'tcp',
-        'from_port': '389',
-        'to_port': '389',
-    },
-    'https': {
-        'name': 'HTTPS',
-        'ip_protocol': 'tcp',
-        'from_port': '443',
-        'to_port': '443',
-    },
-    'smtps': {
-        'name': 'SMTPS',
-        'ip_protocol': 'tcp',
-        'from_port': '465',
-        'to_port': '465',
-    },
-    'imaps': {
-        'name': 'IMAPS',
-        'ip_protocol': 'tcp',
-        'from_port': '993',
-        'to_port': '993',
-    },
-    'pop3s': {
-        'name': 'POP3S',
-        'ip_protocol': 'tcp',
-        'from_port': '995',
-        'to_port': '995',
-    },
-    'ms_sql': {
-        'name': 'MS SQL',
-        'ip_protocol': 'tcp',
-        'from_port': '1433',
-        'to_port': '1433',
-    },
-    'mysql': {
-        'name': 'MYSQL',
-        'ip_protocol': 'tcp',
-        'from_port': '3306',
-        'to_port': '3306',
-    },
-    'rdp': {
-        'name': 'RDP',
-        'ip_protocol': 'tcp',
-        'from_port': '3389',
-        'to_port': '3389',
-    },
+	'all_tcp': {
+		'name': _('All TCP'),
+		'ip_protocol': 'tcp',
+		'from_port': '1',
+		'to_port': '65535',
+	},
+	'all_udp': {
+		'name': _('All UDP'),
+		'ip_protocol': 'udp',
+		'from_port': '1',
+		'to_port': '65535',
+	},
+	'all_icmp': {
+		'name': _('All ICMP'),
+		'ip_protocol': 'icmp',
+		'from_port': '-1',
+		'to_port': '-1',
+	},
+	'ssh': {
+		'name': 'SSH',
+		'ip_protocol': 'tcp',
+		'from_port': '22',
+		'to_port': '22',
+	},
+	'smtp': {
+		'name': 'SMTP',
+		'ip_protocol': 'tcp',
+		'from_port': '25',
+		'to_port': '25',
+	},
+	'dns': {
+		'name': 'DNS',
+		'ip_protocol': 'tcp',
+		'from_port': '53',
+		'to_port': '53',
+	},
+	'http': {
+		'name': 'HTTP',
+		'ip_protocol': 'tcp',
+		'from_port': '80',
+		'to_port': '80',
+	},
+	'pop3': {
+		'name': 'POP3',
+		'ip_protocol': 'tcp',
+		'from_port': '110',
+		'to_port': '110',
+	},
+	'imap': {
+		'name': 'IMAP',
+		'ip_protocol': 'tcp',
+		'from_port': '143',
+		'to_port': '143',
+	},
+	'ldap': {
+		'name': 'LDAP',
+		'ip_protocol': 'tcp',
+		'from_port': '389',
+		'to_port': '389',
+	},
+	'https': {
+		'name': 'HTTPS',
+		'ip_protocol': 'tcp',
+		'from_port': '443',
+		'to_port': '443',
+	},
+	'smtps': {
+		'name': 'SMTPS',
+		'ip_protocol': 'tcp',
+		'from_port': '465',
+		'to_port': '465',
+	},
+	'imaps': {
+		'name': 'IMAPS',
+		'ip_protocol': 'tcp',
+		'from_port': '993',
+		'to_port': '993',
+	},
+	'pop3s': {
+		'name': 'POP3S',
+		'ip_protocol': 'tcp',
+		'from_port': '995',
+		'to_port': '995',
+	},
+	'ms_sql': {
+		'name': 'MS SQL',
+		'ip_protocol': 'tcp',
+		'from_port': '1433',
+		'to_port': '1433',
+	},
+	'mysql': {
+		'name': 'MYSQL',
+		'ip_protocol': 'tcp',
+		'from_port': '3306',
+		'to_port': '3306',
+	},
+	'rdp': {
+		'name': 'RDP',
+		'ip_protocol': 'tcp',
+		'from_port': '3389',
+		'to_port': '3389',
+	},
 }
 
 FLAVOR_EXTRA_KEYS = {
-    'flavor_keys': [
-        ('quota:read_bytes_sec', _('Quota: Read bytes')),
-        ('quota:write_bytes_sec', _('Quota: Write bytes')),
-        ('quota:cpu_quota', _('Quota: CPU')),
-        ('quota:cpu_period', _('Quota: CPU period')),
-        ('quota:inbound_average', _('Quota: Inbound average')),
-        ('quota:outbound_average', _('Quota: Outbound average')),
-    ]
+	'flavor_keys': [
+		('quota:read_bytes_sec', _('Quota: Read bytes')),
+		('quota:write_bytes_sec', _('Quota: Write bytes')),
+		('quota:cpu_quota', _('Quota: CPU')),
+		('quota:cpu_period', _('Quota: CPU period')),
+		('quota:inbound_average', _('Quota: Inbound average')),
+		('quota:outbound_average', _('Quota: Outbound average')),
+	]
 }
--- puppet/modules/openstack-horizon/manifests/init.pp
+++ puppet/modules/openstack-horizon/manifests/init.pp
@@ -1,40 +1,12 @@
-class openstack-horizon {
-
-	package { 'apache2':
-		ensure => installed,
-	}
-
-	package { 'python-memcache':
-		ensure => installed,
-	}
+class openstack-horizon::common {
 
 	package { 'memcached':
 		ensure => installed,
 	}
 
-	package { 'libapache2-mod-wsgi':
+	package { 'python-memcache':
 		ensure => installed,
-	}
-
-	package { 'openstack-dashboard':
-		ensure => installed,
-	}
-
-	package { 'openstack-dashboard-ubuntu-theme':
-		ensure => absent,
-	}
-
-	file { 'local_settings.py':
-		path => '/etc/openstack-dashboard/local_settings.py',
-		ensure => file,
-		source => 'puppet:///modules/openstack-horizon/local_settings.py',
-		owner => root,
-		group => horizon,
-		mode =>	0640,
-		require => [
-			Package['python-memcache'],
-			Package['openstack-dashboard'],
-		],
+		require => Package['memcached'],
 	}
 
 	file { 'memcached.conf':
@@ -51,6 +23,48 @@ class openstack-horizon {
 		ensure => running,
 		enable => true,
 		subscribe => File['memcached.conf'],
+	}
+
+	package { 'apache2':
+		ensure => installed,
+	}
+
+	package { 'libapache2-mod-wsgi':
+		ensure => installed,
+		require => Package['apache2'],
+	}
+
+	file { 'etc:openstack-dashboard':
+		path => '/etc/openstack-dashboard',
+		ensure => directory,
+		owner => root,
+		group => horizon,
+		mode => 0775,
+	}
+
+	# Created with http://strongpasswordgenerator.com.
+	file { 'secret_key':
+		path => '/etc/openstack-dashboard/secret_key',
+		ensure => file,
+		source => 'puppet:///modules/openstack-horizon/secret_key',
+		owner => horizon,
+		group => horizon,
+		mode =>	0600,
+		require => File['etc:openstack-dashboard'],
+	}
+
+	file { 'local_settings.py':
+		path => '/etc/openstack-dashboard/local_settings.py',
+		ensure => file,
+		source => 'puppet:///modules/openstack-horizon/local_settings.py',
+		owner => root,
+		group => horizon,
+		mode =>	0640,
+		require => [
+			Package['python-memcache'],
+			File['etc:openstack-dashboard'],
+			File['secret_key'],
+		],
 	}
 
 	file { 'apache2.conf':
@@ -75,21 +89,145 @@ class openstack-horizon {
 		refreshonly => true,
 	}
 
+}
+
+class openstack-horizon::package inherits openstack-horizon::common {
+
+	package { 'openstack-dashboard':
+		ensure => installed,
+	}
+
+	package { 'openstack-dashboard-ubuntu-theme':
+		ensure => absent,
+	}
+
+	File <| title == 'local_settings.py' |> {
+		require => [
+			Package['python-memcache'],
+			Package['openstack-dashboard'],
+			File['etc:openstack-dashboard'],
+			File['secret_key'],
+		],
+	}
+
+	exec { 'a2enmod:rewrite':
+		command => '/usr/sbin/a2enmod rewrite',
+		creates => '/etc/apache2/mods-enabled/rewrite.conf',
+		require => Package['apache2'],
+	}
+
 	service { 'apache2':
 		ensure => running,
 		enable => true,
 		subscribe => [
+			File['secret_key'],
 			File['local_settings.py'],
-			File['memcached.conf'],
 			File['apache2.conf'],
-			Exec['a2dissite 000-default'],
 			Exec['a2ensite horizon'],
-			#
-			# Check puppet/modules/snmp-manager/manifests/init.pp
-			File['zabbix:apache2.conf'],
-			Exec['a2ensite zabbix'],
+			Exec['a2dissite 000-default'],
+			Exec['a2enmod:rewrite'],
 		],
 	}
 
 }
 
+class openstack-horizon::avos inherits openstack-horizon::common {
+
+	package { 'openstack-dashboard':
+		ensure => absent,
+	}
+
+	package { 'openstack-dashboard-ubuntu-theme':
+		ensure => absent,
+	}
+
+	exec{ 'git:avos':
+		command => 'git clone https://github.com/ciscosystems/avos.git /var/www/html/avos',
+		creates => '/var/www/html/avos/.git',
+		require => [
+			Package['apache2'],
+			Package['openstack-dashboard']
+		],
+	}
+
+	File <| title == 'local_settings.py' |> {
+		require => [
+			Package['python-memcache'],
+			Package['openstack-dashboard'],
+			Exec['git:avos'],
+			File['etc:openstack-dashboard'],
+			File['secret_key'],
+		],
+	}
+
+	file { 'local_setting.py:link':
+		path => '/var/www/html/avos/openstack_dashboard/local/local_settings.py',
+		ensure => link,
+		target => '/etc/openstack-dashboard/local_settings.py',
+		owner => root,
+		group => www-data,
+		require => [
+			File['local_settings.py'],
+			Exec['git:avos'],
+		],
+	}
+	
+	package { [
+		'libpython-dev',
+		'libffi-dev',
+		'libssl-dev',
+		'python-django',
+		'python-pyscss',
+		'python-django-pyscss',
+		]:
+		ensure => installed,
+	}
+
+	exec { 'pip:install:xstatic':
+		command => 'pip install \
+			XStatic \
+			XStatic-Angular \
+			XStatic-Angular-Cookies \
+			XStatic-Angular-Mock \
+			XStatic-Bootstrap-Datepicker \
+			XStatic-Bootstrap-SCSS \
+			XStatic-D3 \
+			XStatic-Font-Awesome \
+			XStatic-Hogan \
+			XStatic-Jasmine \
+			XStatic-jQuery \
+			XStatic-JQuery-Migrate \
+			XStatic-jquery-ui \
+			XStatic-JQuery.quicksearch \
+			XStatic-JQuery.TableSorter \
+			XStatic-JSEncrypt \
+			XStatic-QUnit \ 
+			XStatic-Rickshaw \ 
+			XStatic-Spin',
+		subscribe => Package['python-django'],
+		refreshonly => yes,
+	}
+
+	file { 'avos.conf':
+		path => '/etc/init/avos.conf',
+		
+	}
+
+	service { 'apache2':
+		ensure => stopped,
+		enable => false,
+	}
+
+	service { 'avos':
+		ensure => running,
+		enable => true,
+		subscribe => [
+			File['secret_key'],
+			File['local_settings.py'],
+			File['local_settings.py:link'],
+			Exec['git:avos'],
+			Exec['pip:install:xstatic'],
+		],
+	}
+
+}

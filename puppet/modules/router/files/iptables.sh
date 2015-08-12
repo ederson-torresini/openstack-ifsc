c6189c015c5198a24051dbb4dbe5d3546825ae52
Ederson Torresini <boidacarapreta@gmail.com>
2015-08-12 16:53:42 -0300

    Trove: MySQL as first database image. Since 92575af7c49b5676c08a0366707ad0dd9afbf0f8 (long, long time ago!), I've been working on Trove to provide a DBaaS. In 397f17fa25e2ee8aee61d918705e25b32aa3b209, for the first time I cloud register Trove as a runnable OpenStack service, but with no database image. Until today: MySQL is the first published database image.
    In this configuration, it's mandatory config drive in Nova (overriding 0d973d08e3452b24d46ace5bd177bd2b40d3e2d2).
    
    Next step: database backup.

--- puppet/modules/router/files/iptables.sh
+++ puppet/modules/router/files/iptables.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-# Limpeza das regras antigas
+EXT="eth0"
+HVS="eth1"
+VMS="eth2"
+VMS_NET="200.135.233.0/25"
+VPN_NET="200.135.233.184/29"
+
+# Cleaning rules.
 iptables -t nat -F
 iptables -t filter -F
 #
-# Política padrão
+# Default policies.
 iptables -t nat -P PREROUTING ACCEPT
 iptables -t nat -P POSTROUTING ACCEPT
 iptables -t nat -P OUTPUT ACCEPT
@@ -12,32 +18,47 @@ iptables -t filter -P INPUT   DROP
 iptables -t filter -P FORWARD DROP
 iptables -t filter -P OUTPUT  ACCEPT
 #
-# Manter os estados, liberar loopback (lo) e VPN
+# Default rules, including VPN traffic.
 iptables -t filter -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -t filter -A FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -t filter -A INPUT -i lo -j ACCEPT
 iptables -t filter -A INPUT -s 200.135.233.184/29 -j ACCEPT
 #
-# De externo para roteador: SSH (22/TCP), VPN (500/UDP, 4500/UDP, MASQUERADING), Web (80/TCP, 443/TCP), ping.
-iptables -t filter -A INPUT -i eth0 -p tcp --dport 22 -j ACCEPT
-iptables -t filter -A INPUT -i eth0 -p udp --dport 500 -j ACCEPT
-iptables -t filter -A INPUT -i eth0 -p udp --dport 4500 -j ACCEPT
-iptables -t nat -A POSTROUTING -o eth0 -s 200.135.233.184/29 -j MASQUERADE
-iptables -t filter -A INPUT -i eth0 -p tcp --dport 80 -j ACCEPT
-iptables -t filter -A INPUT -i eth0 -p tcp --dport 443 -j ACCEPT
-iptables -t filter -A INPUT -i eth0 -p icmp --icmp-type echo-request -j ACCEPT
+# From: external;
+# To: router;
+# Services: SSH (22/TCP), VPN (500/UDP, 4500/UDP, MASQUERADING), Web (80/TCP, 443/TCP), ICMP ping.
+iptables -t filter -A INPUT -i ${EXT} -p tcp --dport 22 -j ACCEPT
+iptables -t filter -A INPUT -i ${EXT} -p udp --dport 500 -j ACCEPT
+iptables -t filter -A INPUT -i ${EXT} -p udp --dport 4500 -j ACCEPT
+iptables -t nat -A POSTROUTING -o ${EXT} -s ${VPN_NET} -j MASQUERADE
+iptables -t filter -A INPUT -i ${EXT} -p tcp --dport 80 -j ACCEPT
+iptables -t filter -A INPUT -i ${EXT} -p tcp --dport 443 -j ACCEPT
+iptables -t filter -A INPUT -i ${EXT} -p icmp --icmp-type echo-request -j ACCEPT
 #
-# De externo para virtualizadores: DNS (53/UDP), ping.
-iptables -t filter -A FORWARD -o eth1 -d 200.135.233.201/32 -p udp --dport 53 -j ACCEPT
-iptables -t filter -A FORWARD -o eth1 -d 200.135.233.202/32 -p udp --dport 53 -j ACCEPT
-iptables -t filter -A FORWARD -o eth1 -p icmp --icmp-type echo-request -j ACCEPT
+# From: external;
+# To: hypervisors;
+# Services: DNS (53/UDP), ICMP ping.
+iptables -t filter -A FORWARD -o ${HVS} -d 200.135.233.201/32 -p udp --dport 53 -j ACCEPT
+iptables -t filter -A FORWARD -o ${HVS} -d 200.135.233.202/32 -p udp --dport 53 -j ACCEPT
+iptables -t filter -A FORWARD -o ${HVS} -p icmp --icmp-type echo-request -j ACCEPT
 #
-# De externo para VMs.
-iptables -t filter -A FORWARD -o eth2 -j ACCEPT
+# From: external;
+# To: VMs;
+# Services: any.
+iptables -t filter -A FORWARD -o ${VMS} -j ACCEPT
 #
-# De virtualizadores e VMs para externo.
-iptables -t filter -A FORWARD -o eth0 -j ACCEPT
+# From: hypervisors + VMs;
+# To: external;
+# Services: any.
+iptables -t filter -A FORWARD -o ${EXT} -j ACCEPT
 #
-# De virtualizadores para roteador: SNMP (161/UDP) e Zabbix (10050/TCP).
-iptables -t filter -A INPUT -i eth1 -p udp --dport 161 -j ACCEPT
-iptables -t filter -A INPUT -i eth1 -p tcp --dport 10050 -j ACCEPT
+# From: hypervisors;
+# To: router;
+# Services: SNMP (161/UDP) e Zabbix (10050/TCP).
+iptables -t filter -A INPUT -i ${HVS} -p udp --dport 161 -j ACCEPT
+iptables -t filter -A INPUT -i ${HVS} -p tcp --dport 10050 -j ACCEPT
+#
+# From: VMs;
+# To: hypervisors;
+# Services: RabbitMQ (5672).
+iptables -t filter -A FORWARD -i ${VMS} -s ${VMS_NET} -d 200.135.233.200/32 -p tcp --dport 5672 -j ACCEPT
